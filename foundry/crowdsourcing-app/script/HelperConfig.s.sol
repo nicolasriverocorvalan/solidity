4d200f8a07ee6288622bd7566ac67c9e33b1e1ed
Nicolás Rivero Corvalán <riveronicolas@gmail.com>
2024-02-16 12:34:29 -0300

    Foundry (#10)

---
+++ foundry/crowdsourcing-app/script/HelperConfig.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+
+    // If we are on a local Anvil, we deploy mocks
+    // Otherwise, grab the existing address from live network
+
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaNetworkConfig = NetworkConfig({ 
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306 // ETH / USD
+        });
+        return sepoliaNetworkConfig;
+    }
+
+     function getAnvilEthConfig() public pure returns (NetworkConfig memory sepoliaNetworkConfig) {
+    }
+}
