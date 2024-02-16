5123f9eb5d412cd1aa78c5e87cc14a933f3728a5
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-02-16 09:30:05 -0300

    Foundry

--- foundry/crowdsourcing-app/README.md
+++ foundry/crowdsourcing-app/README.md
@@ -1,4 +1,5 @@
 ## Cheat sheet
 * https://github.com/smartcontractkit/chainlink-brownie-contracts
 * forge install smartcontractkit/chainlink-brownie-contracts@0.8.0 --no-commit
+* forge test -vv
 * 
--- foundry/crowdsourcing-app/test/FundMeTest.t.sol
+++ foundry/crowdsourcing-app/test/FundMeTest.t.sol
@@ -5,7 +5,17 @@ import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
+    FundMe fundMe;
+
     function setUp() external {
-        FundMe fundMe = new FundMe();
-    }    
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(),5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
