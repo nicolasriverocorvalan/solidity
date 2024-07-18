48b78d627314076b8beda1aa1bf6cf150ec64b13
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-07-18 12:29:23 -0300

    advanced foundry

--- 10-account-abstraction/script/SendPackedUserOp.s.sol
+++ 10-account-abstraction/script/SendPackedUserOp.s.sol
@@ -3,23 +3,34 @@
 pragma solidity 0.8.24;
 
 import {Script} from "forge-std/Script.sol";
-import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
+import {PackedUserOperation} from "../lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
-contract DeployMinimal is Script {
-    function run() public {
+contract SendPackedUserOp is Script {
+
+    function run() public {}
+
+    function generateSignedPackedUserOperation(bytes memory _callData, address _sender) public returns (PackedUserOperation memory) {
+        uint256 nonce = vm.getNonce(_sender);
+        PackedUserOperation memory unsignedUserOp = generateUnsignedPackedUserOperation(_callData, _sender, nonce);
 
     }
 
-    function deployMinimalAccount() public returns(HelperConfig, MinimalAccount) {
-        HelperConfig helperConfig = new HelperConfig();
-        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+    function generateUnsignedPackedUserOperation(bytes memory _callData, address _sender, uint256 _nonce) internal pure returns (PackedUserOperation memory) {
+        uint128 verificationGasLimit = 16777216;
+        uint128 callGasLimit = verificationGasLimit;
+        uint128 maxPriorityFeePerGas = 256;
+        uint128 maxFeePerGas = maxPriorityFeePerGas;
 
-        vm.startBroadcast(config.account);
-        MinimalAccount minimalAccount = new MinimalAccount(config.entryPoint);
-        minimalAccount.transferOwnership(msg.sender);
-        vm.stopBroadcast();
-        
-        return (helperConfig, minimalAccount);
+        return PackedUserOperation({
+            sender: _sender,
+            nonce: _nonce,
+            initCode: hex"",
+            callData: _callData,
+            accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
+            preVerificationGas: verificationGasLimit,
+            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
+            paymasterAndData: hex"",
+            signature: hex""
+        });
     }
 }
--- 10-account-abstraction/test/ethereum/MinimalAccountTest.t.sol
+++ 10-account-abstraction/test/ethereum/MinimalAccountTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {MinimalAccount} from "../../src/ethereum/MinimalAccount.sol";
-import {DeployMinimal} from "../../script/SendPackedUserOp.s.sol";
+import {DeployMinimal} from "../../script/DeployMinimal.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
@@ -12,6 +12,8 @@ contract MinimalAccountTest is Test {
     HelperConfig helperConfig;
     MinimalAccount minimalAccount;
     ERC20Mock usdc;
+
+    address randomAddress = makeAddr("randomAddress");
 
     uint256 constant AMOUNT = 1e18;
 
@@ -35,5 +37,17 @@ contract MinimalAccountTest is Test {
         // Assert
         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
     }
-}
 
+    function testNonOwnerCannotExecuteCommands() public {
+        // Arrange
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory data = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+
+        // Act
+        vm.prank(randomAddress);
+        vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
+        minimalAccount.execute(dest, value, data);
+    }
+}
