9b401e79fb3de56c98132d4b9626c8eb42e4a91f
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-09-25 11:58:55 -0300

    foundry

--- airdrop-and-signatures/test/AirdropTest.t.sol
+++ airdrop-and-signatures/test/AirdropTest.t.sol
@@ -12,6 +12,7 @@ contract AirdropTest is Test {
     ElBarto public token;
     address public userAccount;
     uint256 public userPrivKey;
+    address public gasPayer;
 
     bytes32 public ROOT=0xa3380204c782abb3523e8e685d8c4f3425c3f721593cea2e22dd5bce518de0ba;
     uint256 public AMOUNT= 25 * 1e18;
@@ -28,13 +29,19 @@ contract AirdropTest is Test {
         token.transfer(address(airdrop), AMOUNT_TO_MINT);
         (userAccount, userPrivKey) = makeAddrAndKey("user");
         console.log("User address: ", userAccount); //user address obtained: 0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testCanClaim() public {
         uint256 startingBalance = token.balanceOf(userAccount);
+        bytes32 digest = airdrop.getDigest(userAccount, AMOUNT);
 
-        vm.prank(userAccount);
-        airdrop.claim(userAccount, AMOUNT, PROOF);
+        // sign a message
+        (uint8 v,bytes32 r,bytes32 s)= vm.sign(userPrivKey, digest);
+    
+        // gasPayer calls claim using the signed message
+        vm.prank(gasPayer);
+        airdrop.claim(userAccount, AMOUNT, PROOF, v, r, s);
 
         uint256 endingBalance = token.balanceOf(userAccount);
         console.log("Starting balance: %d, Ending balance: %d", startingBalance, endingBalance);
